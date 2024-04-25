d86d6cb0a9c04eb45d9e71e82aa1128940c36240
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-04-25 18:17:41 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const address =
+      "00110001 00110100 00110000 00100000 00111000 00110010 00100000 00110001 00110001 00110110 00100000 00110100";
+    window.localStorage.setItem("interesting stuff", address);
+  }, []);
   return (
     <>
-      <div>
-        Good to see you here!
-        </div>
-        <img src="question.png" class = "hide">
-        </img>
+      <div>Good to see you here!</div>
+      <img src="question.png" class="hide"></img>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
