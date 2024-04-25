d065b54cbad703d97521f390175d66b5b9d60970
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-04-25 18:39:39 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,9 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    const address =
-      "00110001 00110100 00110000 00100000 00111000 00110010 00100000 00110001 00110001 00110110 00100000 00110100";
-    window.localStorage.setItem("interesting stuff", address);
-  }, []);
   return (
     <>
-      <div>Good to see you here!</div>
+      <div>Good to see you here</div>
       <img src="question.png" class="hide"></img>
     </>
   );
